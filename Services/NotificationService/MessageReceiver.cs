8fd77a9047aaa1d95f5a9e024c96704ff74a5720
Joby Else <113983712+Jobster9@users.noreply.github.com>
2024-11-14 22:20:34 +0000

    refactored message receivers
    cut down on code duplication present in message receivers

--- Services/NotificationService/MessageReceiver.cs
+++ Services/NotificationService/MessageReceiver.cs
@@ -1,70 +1,27 @@
 using Microsoft.Extensions.Options;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
-using System.Text;
 using Common.Configuration;
 using Common.Constants;
+using Common;
 using Services.NotificationService.Handlers;
 
 namespace Services.NotificationService;
-public class NotificationMessageReceiver : BackgroundService
+public class NotificationMessageReceiver : BaseMessageReceiver
 {
-    private readonly RabbitMQConfig _config;
-    private static readonly string[] NotificationTypes =[
+    private static readonly string[] NotificationTypes =
+    [
         MessageTypes.Notifications.Login,
         MessageTypes.Notifications.Logout,
         MessageTypes.Notifications.PasswordReset,
         MessageTypes.Notifications.ProfileUpdate
     ];
 
-    
-    public NotificationMessageReceiver(IOptions<RabbitMQConfig> options)
+    public NotificationMessageReceiver(IOptions<RabbitMQConfig> options) 
+        : base(options, NotificationTypes)
     {
-        _config = options.Value;
     }
-    
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+
+    protected override async Task HandleMessage(string messageType, string message)
     {
-        var factory = new ConnectionFactory 
-        { 
-            HostName = _config.HostName,
-            Port = _config.Port,
-            UserName = _config.UserName,
-            Password = _config.Password
-        };
-
-        using var connection = await factory.CreateConnectionAsync();
-        using var channel = await connection.CreateChannelAsync();
-
-        await channel.ExchangeDeclareAsync(_config.ExchangeName, ExchangeType.Direct);
-        var queueName = (await channel.QueueDeclareAsync()).QueueName;
-        
-        foreach(var type in NotificationTypes)
-        {
-            await channel.QueueBindAsync(queueName, _config.ExchangeName, type);
-            Console.WriteLine($"Bound to {type} notifications");
-        }
-
-        var consumer = new AsyncEventingBasicConsumer(channel);
-        consumer.ReceivedAsync += async (model, ea) =>
-        {
-            var body = ea.Body.ToArray();
-            var message = Encoding.UTF8.GetString(body);
-            var notificationType = ea.RoutingKey;
-
-            await NotificationHandler.HandleNotification(notificationType, message);
-        };
-
-        await channel.BasicConsumeAsync(queueName, true, consumer);
-
-        // Keep the service running until cancellation is requested
-        try
-        {
-            await Task.Delay(Timeout.Infinite, stoppingToken);
-        }
-        catch (OperationCanceledException)
-        {
-            // Graceful shutdown
-        }
+        await NotificationHandler.HandleNotification(messageType, message);
     }
 }
