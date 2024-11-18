1b6b41596ec1fb006429c22851ed38b249290534
Philip Sykes <philipsykes2004@icloud.com>
2024-11-18 14:11:52 +0000

    Reformatted SearchResultListComponent.razor to take BsonDocument parameter. Added all genres and limited genre suggestions and genre tags on results, fixed validation on form submission

--- Blazor/Pages/MediaSearchModel.cs
+++ Blazor/Pages/MediaSearchModel.cs
@@ -1,12 +1,26 @@
 namespace Blazor.Models;
+using System.ComponentModel.DataAnnotations;
 
+
+public class AtLeastOneFieldRequiredAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var model = (MediaSearchModel)validationContext.ObjectInstance;
+
+        if (string.IsNullOrWhiteSpace(model.mediaTitle) && !model.MediaGenres.Any())
+        {
+            return new ValidationResult("At least one field must be filled.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
+
+[AtLeastOneFieldRequired]
 public class MediaSearchModel
 {
     public string mediaTitle { get; set; }
     public List<string> MediaGenres { get; set; } = new List<string>();
-    
-    public bool IsValid =>
-    !string.IsNullOrWhiteSpace(mediaTitle) ||
-    !MediaGenres.Any();
     
 }
