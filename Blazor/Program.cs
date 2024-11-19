d5b72ababfef88ca7a06a0f450b562ba600a8971
Philip Sykes <philipsykes2004@icloud.com>
2024-11-19 12:23:56 +0000

    Added Email System, Currently only uses inline html, Cannot be Called directly in blazor

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -9,6 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7500") });
+builder.Services.AddScoped<Common.Notification.Email.EmailService>();
+
 
 
 await builder.Build().RunAsync();
--- Common/Notification/Email/EmailService.cs
+++ Common/Notification/Email/EmailService.cs
@@ -11,6 +11,8 @@ public class EmailService
     private readonly string _senderEmail = "hallam.amls@gmail.com";
     private readonly string _appPassword = "dktpdtqizcxwjerg";
 
+    //TODO: Remove inline HTML for E-mail Content
+
     private async Task SendEmailAsync(string toEmail, string subject, string body, bool isBodyHtml = true)
     {
         try
@@ -41,10 +43,10 @@ public class EmailService
 
     public async Task SendBorrowEmailAsync(string toEmail, Dictionary<string, string> data)
     {
+        string htmlBody = "<!DOCTYPE html>\n<html>\n<head>\n    <style>\n        body { font-family: Arial, sans-serif; }\n    </style>\n</head>\n<body>\n\n<h1>Hello {UserName}!</h1>\n<h3>This is to confirm your booking of {Media}</h3>\n\n<p>You can pick up your item(s) at {Time} from {Location}</p>\n\n</body>\n</html>";
+            
         try
         {
-            string htmlBody = await File.ReadAllTextAsync("Content/borrow-email.html");
-
             foreach (var item in data)
             {
                 htmlBody = htmlBody.Replace($"{{{item.Key}}}", item.Value);
