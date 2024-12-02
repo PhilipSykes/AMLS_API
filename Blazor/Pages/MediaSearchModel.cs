933c9ff2181c6c2d3edfdb069fdf230f0f959c05
Philip Sykes <philipsykes2004@icloud.com>
2024-12-02 13:06:47 +0000

    Styled login page and stopped navbar displaying on it

--- Blazor/Pages/MediaSearchModel.cs
+++ Blazor/Pages/MediaSearchModel.cs
@@ -1,6 +1,6 @@
-namespace Blazor.Models;
 using System.ComponentModel.DataAnnotations;
 
+namespace Blazor.Models;
 
 public class AtLeastOneFieldRequiredAttribute : ValidationAttribute
 {
@@ -9,11 +9,8 @@ public class AtLeastOneFieldRequiredAttribute : ValidationAttribute
         var model = (MediaSearchModel)validationContext.ObjectInstance;
 
         if (string.IsNullOrWhiteSpace(model.MediaTitle) && !model.MediaGenres.Any())
-        {
             return new ValidationResult("At least one field must be filled.");
-        }
         return ValidationResult.Success;
-
     }
 }
 
@@ -21,7 +18,7 @@ public class AtLeastOneFieldRequiredAttribute : ValidationAttribute
 public class MediaSearchModel
 {
     public string? MediaTitle { get; set; }
-    public List<string> MediaGenres { get; set; } = new List<string>();
-    public Dictionary<string, List<string>> MediaPublisher { get; set; } = new Dictionary<string, List<string>>();
-    public Dictionary<string, List<string>> MediaAuthor { get; set; } = new Dictionary<string, List<string>>();
+    public List<string> MediaGenres { get; set; } = new();
+    public Dictionary<string, List<string>> MediaPublisher { get; set; } = new();
+    public Dictionary<string, List<string>> MediaAuthor { get; set; } = new();
 }
