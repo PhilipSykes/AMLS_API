a2bd4445281817079841c970eea5f5bed64e9a43
billyjimmy <91666138+billyjimmy@users.noreply.github.com>
2024-12-16 17:26:20 +0000

    Added controller

--- ReservationService/LoansController.cs
+++ ReservationService/LoansController.cs
@@ -1,6 +1,44 @@
-namespace Api.Controllers;
+using Common.Constants;
+using Common.MessageBroker;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class LoansController
+namespace ReservationService;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize(Policy = Policies.CanBorrowMedia)]
+public class LoansController : ControllerBase
 {
-    //TODO: borrowing & returning would go in here
+    private readonly Exchange _exchange;
+    private readonly ILoanManager _loanManager;
+    
+    /// <summary>
+    /// Initializes a new instance of the LoansController
+    /// </summary>
+    /// <param name="exchange">Message broker exchange service</param>
+    /// <param name="loanManager">Loan manager service</param>
+    public LoansController(Exchange exchange, ILoanManager loanManager)
+    {
+        _exchange = exchange;
+        _loanManager = loanManager;
+    }
+    
+    [HttpPost("check-in")]
+    public async Task<ActionResult> CheckIn(string physicalId)
+    {
+        
+        var result = await _loanManager.CheckIn(physicalId);
+    
+        return Ok(new { message = result.StatusCode });
+    }
+    
+    [HttpPost("check-out")]
+    public async Task<ActionResult> CheckOut(string physicalId, string memberId)
+    {
+        
+        var result = await _loanManager.CheckOut(physicalId, memberId);
+    
+        return Ok(new { message = result.StatusCode });
+    }
 }
